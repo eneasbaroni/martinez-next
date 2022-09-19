c2881664f2fa4339bee3f9b37c3e8a05d59797be
Eneas Baroni <eneasbaroni@gmail.com>
2022-09-18 22:29:11 -0300

    inicializadon

---
+++ db/data.js
@@ -0,0 +1,72 @@
+const db = [
+  {
+    nombre: "La Rufina",
+    año: "2022",
+    tipo: "Casa de Familia",
+    seleccionada: "true",
+    img: "/images/prueba01.jpg"    
+  },
+  {
+    nombre: "El Terron",
+    año: "2021",
+    tipo: "Casa de Familia",
+    seleccionada: "true",
+    img: "/images/prueba02.jpg"    
+  },
+  {
+    nombre: "Casa Lopez",
+    año: "2020",
+    tipo: "Casa de Familia",
+    seleccionada: "true",
+    img: "/images/prueba04.jpg"   
+  },
+  {
+    nombre: "Casa Rocanto",
+    año: "2020",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba05.jpg"    
+  },
+  {
+    nombre: "Gianuzzo",
+    año: "2021",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba03.jpg"    
+  },
+  {
+    nombre: "El Pueblo",
+    año: "2021",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba06.jpg"    
+  },
+  {
+    nombre: "Tarraubella",
+    año: "2020",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba07.jpg"    
+  },
+  {
+    nombre: "Casa Salomon",
+    año: "2020",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba08.jpg"    
+  },
+  {
+    nombre: "Punta Peñon",
+    año: "2020",
+    tipo: "Casa de Familia",
+    seleccionada: "false",
+    img: "/images/prueba09.jpg"    
+  },
+]
+
+export default db
+
+
+
+
+
