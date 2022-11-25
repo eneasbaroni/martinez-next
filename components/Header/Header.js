822ac382b87858dcc011017bf3506969834764f0
Eneas Baroni <eneasbaroni@gmail.com>
2022-11-25 00:46:25 -0300

    copy

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useEffect, useState } from "react"
+import { useRouter } from 'next/router'
 
 
 const Header = () => {
@@ -8,6 +9,8 @@ const Header = () => {
   const [windowSize, setWindowSize] = useState()
   const [navbarOpen, setNavbarOpen] = useState(false)
   const [position, setposition] = useState("-100vw")
+
+  const router = useRouter();  
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -46,10 +49,10 @@ const Header = () => {
             <div className="menuIcon"><Image className="menuIcon" width={25} height={25} src='/images/menu.svg' alt="menu" onClick={openMenu}/></div>      
           :
             <>
-              <Link href={"/"}  className="navItem">INICIO</Link>
-              <Link href={"/obras"}  className="navItem">OBRAS</Link>
-              <Link href={"/nosotros"}  className="navItem">NOSOTROS</Link>
-              <Link href={"/contacto"}  className="navItem">CONTACTO</Link>
+              <li className={router.pathname == "/" ? "navItemActive" : "navItem"}><Link href={"/"}>INICIO</Link></li>
+              <li className={router.pathname == "/obras" ? "navItemActive" : "navItem"}><Link href={"/obras"}>OBRAS</Link></li>
+              <li className={router.pathname == "/nosotros" ? "navItemActive" : "navItem"}><Link href={"/nosotros"}  className="navItem">NOSOTROS</Link></li>
+              <li className={router.pathname == "/contacto" ? "navItemActive" : "navItem"}><Link href={"/contacto"}  className="navItem">CONTACTO</Link></li>
             </>   
         } 
         
