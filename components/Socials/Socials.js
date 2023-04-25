e3b6cd6bccff2025577424f2adbf815e46f5c47d
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-04-25 13:43:10 -0300

    socials

--- components/Socials/Socials.js
+++ components/Socials/Socials.js
@@ -7,7 +7,7 @@ const Socials = () => {
     <div className="socialsContainer">
       <a href={"https://www.instagram.com/martinezrubiano.construcciones"} rel="noreferrer"  target="_blank"><div className="mediaIcon"><Image layout="fill"  src='/images/instaIcon.svg' alt="instaIcon"/></div></a>
       <a href={"https://www.facebook.com/people/Martinez-Rubiano-Construcciones-SAS/100063763683411/"}><div className="mediaIcon"><Image layout="fill"  src='/images/faceIcon.svg' alt="faceIcon"/></div></a>
-      <a href={"https://wa.me/+5493534203441"}><div className="mediaIcon"><Image layout="fill"  src='/images/waIcon.svg' alt="waIcon"/></div></a>
+      <a href={"https://wa.me/+5493534203441"} rel="noreferrer"  target="_blank"><div className="mediaIcon"><Image layout="fill"  src='/images/waIcon.svg' alt="waIcon"/></div></a>
     </div>
   )
 }
